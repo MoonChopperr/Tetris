da5fccc5d21227e39ae8db9ba935f50fbadd45bb
Ryou <nishiyamaryou@gmail.com>
2024-05-08 02:11:11 -0400

    clear rows on fill

--- Code/game.py
+++ Code/game.py
@@ -70,11 +70,33 @@ class Game:
                 self.timers['horizontal move'].activate()
 
     def create_new_tetromino(self):
+        self.check_finished_rows()
         self.tetromino = Tetromino(
             choice(list(TETROMINOS.keys())),
             self.sprites,
             self.create_new_tetromino,
             self.field_data)
+
+    def check_finished_rows(self):  #check if row is filled with blocks
+        #get row indexes
+        delete_rows = []
+        for i, row in enumerate(self.field_data):
+            if all(row):
+                delete_rows.append(i)
+        if delete_rows:
+            for delete_row in delete_rows:
+                #delete full rows
+                for block in self.field_data[delete_row]:
+                    block.kill()
+                #move down blocks
+                for row in self.field_data:
+                    for block in row:
+                        if block and block.pos.y < delete_row:
+                            block.pos.y +=1
+            #rebuild field data
+            self.field_data = [[0 for x in range(COLUMNS)] for y in range(ROWS)]
+            for block in self.sprites:
+                self.field_data[int(block.pos.y)][int(block.pos.x)] = block
     def run(
         self,
     ):  # block image transfer = one surface ontop of another surface 2 args (surface, position(x,y))
