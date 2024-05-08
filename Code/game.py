3ebe5398e54d24ab1ecec863f2c853da1b01f761
Ryou <nishiyamaryou@gmail.com>
2024-05-08 01:59:33 -0400

    collisions

--- Code/game.py
+++ Code/game.py
@@ -15,18 +15,21 @@ class Game:
         # lines layer where we adjust the alpha opacity
         self.line_surface = self.surface.copy()  # copy of above
         self.line_surface.fill((0, 255, 0))  # green
-        self.line_surface.set_colorkey(
-            (0, 255, 0)
-        )  # sets color key to green hides green
+        self.line_surface.set_colorkey((0, 255, 0))  # sets color key to green hides green
         self.line_surface.set_alpha(30)  # max 255, opacity, higher = less transparent
 
         #tetromino
-        self.tetromino = Tetromino(choice(list(TETROMINOS.keys())), self.sprites)
-
+        self.field_data=[[0 for x in range(COLUMNS)] for y in range(ROWS)]
+        self.tetromino = Tetromino(
+            choice(list(TETROMINOS.keys())),
+            self.sprites,
+            self.create_new_tetromino,
+            self.field_data)
 
         #timer
         self.timers={
-            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
+            'horizontal move': Timer(MOVE_WAIT_TIME),
         }
         #test
         # self.block = Block(self.sprites,pygame.Vector2(3,5), 'blue')
@@ -56,11 +59,26 @@ class Game:
         self.surface.blit(self.line_surface, (0, 0))
         # Pygame blit() The Pygame blit() method is one of the methods to place an image onto the screens of pygame applications. It intends to put an image on the screen. It just copies the pixels of an image from one surface to another surface just like that.
 
+    def input(self):
+        keys=pygame.key.get_pressed() #built in ds returns for all possible inputs
+        if not self.timers['horizontal move'].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers['horizontal move'].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
 
+    def create_new_tetromino(self):
+        self.tetromino = Tetromino(
+            choice(list(TETROMINOS.keys())),
+            self.sprites,
+            self.create_new_tetromino,
+            self.field_data)
     def run(
         self,
     ):  # block image transfer = one surface ontop of another surface 2 args (surface, position(x,y))
-
+        self.input()
         #update timer
         self.timer_update()
         self.sprites.update()
@@ -73,16 +91,42 @@ class Game:
         pygame.draw.rect(self.display_surface, (255, 255, 255), self.rect, 1)  # border
 
 class Tetromino: #organizes multiple blocks into a shape
-    def __init__(self, shape, group):
+    def __init__(self, shape, group, create_new_tetromino, field_data):
         #setup
         self.block_positions = TETROMINOS[shape]['shape']
         self.color=TETROMINOS[shape]['color']
-
+        self.create_new_tetromino = create_new_tetromino
+        self.field_data=field_data
         #create blocks
         self.blocks = [Block(group, pos, self.color) for pos in self.block_positions] #list comprehension of creating a block instance of each block
-    def move_down(self):
+    def move_horizontal(self, amount):
         for block in self.blocks:
-            block.pos.y += 1
+            block.pos.x += amount
+
+    def move_down(self):
+        if not self.next_move_vertical_collide(self.blocks, 1):
+            for block in self.blocks:
+                block.pos.y += 1
+        else:
+            for block in self.blocks:
+                self.field_data[int(block.pos.y)][int(block.pos.x)] = 1
+            self.create_new_tetromino()
+
+    #collisions if piece on the farthest right of window, aka below 0 do not allow movement
+    def next_move_horizontal_collide(self, blocks, amount):
+        #imaginery tetrimono
+        collision_list = [block.horizontal_collide(int(block.pos.x + amount), self.field_data) for block in self.blocks]
+        return True if any(collision_list) else False
+    #movement
+    def move_horizontal(self, amount):
+        if not self.next_move_horizontal_collide(self.blocks, amount):
+            for block in self.blocks:
+                block.pos.x += amount
+
+    def next_move_vertical_collide(self, blocks, amount):
+        collision_list = [block.vertical_collide(int(block.pos.y+ amount), self.field_data) for block in self.blocks]
+        return True if any(collision_list)else False
+
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, group, pos, color):
@@ -97,6 +141,16 @@ class Block(pygame.sprite.Sprite):
         # y= self.pos.y * CELL_SIZE
         self.rect = self.image.get_rect(topleft=self.pos * CELL_SIZE)
 
+    def horizontal_collide(self, x, field_data):
+        if not 0 <= x < COLUMNS:
+            return True
+        if field_data[int(self.pos.y)][x]:
+            return True
+    def vertical_collide(self, y, field_data):
+        if y >= ROWS:
+            return True
+        if y >=0 and field_data[y][int(self.pos.x)]:
+            return True
     def update(self):
         # print(self.pos)
         self.pos * 40
