a453b218c41d9baa5d65ec1d0705ab3fa9309716
Ryou <nishiyamaryou@gmail.com>
2024-05-08 03:06:04 -0400

    eod debugging clear row crash

--- Code/game.py
+++ Code/game.py
@@ -30,6 +30,7 @@ class Game:
         self.timers={
             'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
             'horizontal move': Timer(MOVE_WAIT_TIME),
+            'rotate': Timer(ROTATE_WAIT_TIME)
         }
         #test
         # self.block = Block(self.sprites,pygame.Vector2(3,5), 'blue')
@@ -69,6 +70,13 @@ class Game:
                 self.tetromino.move_horizontal(1)
                 self.timers['horizontal move'].activate()
 
+
+        #rotation input
+        if not self.timers['rotate'].active:
+            if keys[pygame.K_UP]:
+                self.tetromino.rotate()
+                self.timers['rotate'].activate()
+
     def create_new_tetromino(self):
         self.check_finished_rows()
         self.tetromino = Tetromino(
@@ -85,9 +93,11 @@ class Game:
                 delete_rows.append(i)
         if delete_rows:
             for delete_row in delete_rows:
+
                 #delete full rows
                 for block in self.field_data[delete_row]:
                     block.kill()
+                    # print('cleared row')
                 #move down blocks
                 for row in self.field_data:
                     for block in row:
@@ -119,6 +129,7 @@ class Tetromino: #organizes multiple blocks into a shape
         self.color=TETROMINOS[shape]['color']
         self.create_new_tetromino = create_new_tetromino
         self.field_data=field_data
+        self.shape = shape
         #create blocks
         self.blocks = [Block(group, pos, self.color) for pos in self.block_positions] #list comprehension of creating a block instance of each block
     def move_horizontal(self, amount):
@@ -149,7 +160,21 @@ class Tetromino: #organizes multiple blocks into a shape
         collision_list = [block.vertical_collide(int(block.pos.y+ amount), self.field_data) for block in self.blocks]
         return True if any(collision_list)else False
 
-
+    def rotate(self):
+        if self.shape != 'O':
+            pivot_pos = self.blocks[0].pos
+            new_block_positions = [block.rotate(pivot_pos) for block in self.blocks]
+            #collision check
+            for pos in new_block_positions:
+                if pos.x < 0 or pos.x >= COLUMNS:
+                    return
+                if self.field_data[int(pos.y)][int(pos.x)]:
+                    return
+                if pos.y > ROWS:
+                    return
+            #new pos
+            for i, block in enumerate(self.blocks):
+                block.pos = new_block_positions[i]
 class Block(pygame.sprite.Sprite):
     def __init__(self, group, pos, color):
         super().__init__(group)
@@ -162,7 +187,11 @@ class Block(pygame.sprite.Sprite):
         # x= self.pos.x * CELL_SIZE # needed to per grid
         # y= self.pos.y * CELL_SIZE
         self.rect = self.image.get_rect(topleft=self.pos * CELL_SIZE)
-
+    def rotate(self, pivot_pos):
+        distance = self.pos - pivot_pos
+        rotated = distance.rotate(90)
+        new_pos = pivot_pos + rotated
+        return new_pos
     def horizontal_collide(self, x, field_data):
         if not 0 <= x < COLUMNS:
             return True
